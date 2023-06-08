65f679988c9395188b7172f5a189bb034acde20c
wei <12786422+wei246@user.noreply.gitee.com>
2023-06-08 09:04:58 +0800

    测试

--- src/main/java/edu/czjt/reggie/ReggieTakeOutApplication.java
+++ src/main/java/edu/czjt/reggie/ReggieTakeOutApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @Slf4j
 @SpringBootApplication
